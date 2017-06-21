f5ad6f63ae6e054006a2f803334f6bf191842f6f
Aron Bordin <aron.bordin@gmail.com>
2017-06-21 01:39:08 -0300

    added travis

---
+++ cogitare/utils/tests.py
@@ -0,0 +1,6 @@
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+__all__ = ['mock']
--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,25 +1,58 @@
 import unittest
-from cogitare import utils, config
+import mock as m
+from cogitare import utils
+import pytest
 
 
 class TestUtils(unittest.TestCase):
 
-    def test_verbosity_default(self):
-        self.assertEqual(utils.get_verbosity(), config['VERBOSE'])
-
-    def test_verbosity_set(self):
-        self.assertEqual(utils.get_verbosity(True), True)
-        self.assertEqual(utils.get_verbosity(False), False)
-
-    def test_verbosity_set_boolonly(self):
-        self.assertRaises(TypeError, utils.get_verbosity('test'))
-
     def test_call_feedback_raises(self):
-        self.assertRaisesRegexp(ValueError, 'callable nor list', utils.call_feedback, 'test')
+        with pytest.raises(ValueError) as info:
+            utils.call_feedback('test')
+        self.assertIn('callable nor list', str(info.value))
 
         def f():
             return 'test'
 
-        self.assertRaisesRegex(ValueError, 'callable nor list', utils.call_feedback, [f, 'test'])
-        self.assertRaisesRegex(ValueError, 'callable nor list',
-                               utils.call_feedback, [f, [f, f, 'test']])
+        with pytest.raises(ValueError) as info:
+            utils.call_feedback([f, 'test'])
+        self.assertIn('callable nor list', str(info.value))
+
+        with pytest.raises(ValueError) as info:
+            utils.call_feedback([f, [f, f, 'test']])
+        self.assertIn('callable nor list', str(info.value))
+
+    def test_call_feedback_ignores_empty(self):
+        out = utils.call_feedback(None)
+        self.assertEqual(out, False)
+
+    def test_call_feedback_call(self):
+        def function(*args, **kwargs):
+            pass
+
+        f = m.create_autospec(function, return_value=None)
+        utils.call_feedback(f)
+        f.assert_any_call()
+
+    def test_call_feedback_list_call(self):
+        def f1(*args, **kwargs):
+            pass
+
+        def f2(*args, **kwargs):
+            pass
+
+        def f3(*args, **kwargs):
+            pass
+
+        def f4(*args, **kwargs):
+            pass
+
+        def f5(*args, **kwargs):
+            pass
+
+        fs = [m.create_autospec(f, return_value=None) for f in [f1, f2, f3, f4, f5]]
+
+        feedbacks = [fs[0], fs[1], [fs[2], [fs[3], fs[4]]], fs[0]]
+        utils.call_feedback(feedbacks)
+        for f in fs:
+            f.assert_any_call()
