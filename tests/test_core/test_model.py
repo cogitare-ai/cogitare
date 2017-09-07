8d15d39cd9c1602c1e2258876033f216921c8ae9
Aron Bordin <aron.bordin@gmail.com>
2017-09-07 01:56:56 -0300

    fix tests

--- tests/test_core/test_model.py
+++ tests/test_core/test_model.py
@@ -1,7 +1,4 @@
-import torch
-import pytest
 from cogitare import Model
-from tests.common import TestCase
 
 
 class Model1(Model):
@@ -11,31 +8,3 @@ class Model1(Model):
 
     def loss(self, x):
         pass
-
-
-class TestModel(TestCase):
-
-    def test_to_tensor(self):
-        m = Model1(cuda=False)
-
-        v = [1, 2, 3]
-        self.assertEqual(m._to_tensor(v), torch.LongTensor(v))
-
-        v = [1.0, 2.0, 3.0]
-        self.assertEqual(m._to_tensor(v), torch.DoubleTensor(v))
-
-        v = [torch.LongTensor([1, 2, 3]), torch.LongTensor([4, 5, 6])]
-        vv = torch.LongTensor([[1, 2, 3], [4, 5, 6]])
-        self.assertEqual(m._to_tensor(v), vv)
-
-        v = ['as', 'cc']
-        with pytest.raises(ValueError) as info:
-            m._to_tensor(v)
-        self.assertIn('Invalid data type:', str(info.value))
-
-        v = [1, 2, 3]
-        types = [torch.ByteTensor, torch.DoubleTensor, torch.FloatTensor, torch.LongTensor]
-
-        for t in types:
-            vv = t(v)
-            self.assertEqual(type(m._to_tensor(v, t)), type(vv))
