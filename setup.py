73493b23452380259b8d108b02a6ce3e7b0d0bca
Aron Bordin <aron.bordin@gmail.com>
2017-09-12 15:46:44 -0300

    add fork-me tag
    add forkme

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='cogitare',
-    version='0.1',
-    url='https://github.com/aron-bordin/cogitare',
+    version='0.1.1',
+    url='https://github.com/cogitare-ai/cogitare',
     description=' Cogitare - A Modern, Powerful, and Modular Deep Learning and Machine Learning framework in Python.',
     author='Aron Bordin',
     packages=find_packages(exclude=('tests', 'tests.*')),
