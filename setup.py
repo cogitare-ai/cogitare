6e9b6e347a80153a283134ad9628efb82a6ba8c7
Aron Bordin <aron.bordin@gmail.com>
2018-02-01 13:26:18 -0200

    update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='cogitare',
-    version='0.1.1',
+    version='0.1.0',
     url='https://github.com/cogitare-ai/cogitare',
     description=' Cogitare - A Modern, Powerful, and Modular Deep Learning and Machine Learning framework in Python.',
     author='Aron Bordin',
+    author_email='aron.bordin@gmail.com',
+    keywords=['deep learning', 'framework', 'PyTorch'],
     packages=find_packages(exclude=('tests', 'tests.*')),
 )
